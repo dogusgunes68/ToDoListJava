8452c83e0d7c5560bfec7c66b29b068dc1ab060e
Dogus Gunes <dogusgunes68@gmail.com>
2022-05-03 21:29:01 +0300

    warning popup removed

--- app/src/main/java/com/example/todolistjava/view/AddToDoListFragment.java
+++ app/src/main/java/com/example/todolistjava/view/AddToDoListFragment.java
@@ -1,36 +1,25 @@
 package com.example.todolistjava.view;
 
 import android.app.Dialog;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridLayout;
-import android.widget.PopupMenu;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.todolistjava.R;
 import com.example.todolistjava.adapter.ColorRecyclerAdapter;
-import com.example.todolistjava.adapter.WarningReyclerAdapter;
 import com.example.todolistjava.databinding.FragmentAddToDoListBinding;
-import com.example.todolistjava.model.ToDo;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 public class AddToDoListFragment extends Fragment {
@@ -65,13 +54,6 @@ public class AddToDoListFragment extends Fragment {
             }
         });
 
-        fragmentBinding.warningImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showWarningPopup();
-            }
-        });
-
     }
 
     public void showColorsPopup(){
@@ -101,33 +83,5 @@ public class AddToDoListFragment extends Fragment {
         myDialog.show();
     }
 
-    public void showWarningPopup(){
-        HashMap<String, String> warningList = new HashMap<>();
-        warningList.put("Very Important","#DF0808");
-        warningList.put("Important","#DDCB00");
-        warningList.put("Not Neccessary","#5EC606");
 
-        List<String> keys = new ArrayList<>();
-
-        for (String key : warningList.keySet()){
-            keys.add(key);
-        }
-
-        myDialog.setContentView(R.layout.warning_popup);
-        RecyclerView warningRecyclerView = myDialog.findViewById(R.id.warningRecyclerView);
-        TextView closeText = myDialog.findViewById(R.id.warningCloseTextView);
-        WarningReyclerAdapter adapter = new WarningReyclerAdapter(warningList,keys,fragmentBinding.warningImageView,myDialog);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
-        warningRecyclerView.setLayoutManager(linearLayoutManager);
-        warningRecyclerView.setAdapter(adapter);
-
-        closeText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myDialog.dismiss();
-            }
-        });
-
-        myDialog.show();
-    }
 }
